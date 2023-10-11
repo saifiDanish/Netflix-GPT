0b6a77759fbbd9fa3d47afb5c11c91f2a2789804
Mohd Danish Saifi <saifidanish549@gmail.com>
2023-10-12 00:14:17 +0530

    Sign In/Sign Up page and Header Completed

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,6 +1,11 @@
+import { useState } from "react"
 import Header from "./Header"
 
 const Login = () => {
+  const [isSignIn,setIsSignIn] =useState(true);
+  const toggleSignUp = ()=>{
+    setIsSignIn(!isSignIn)
+  }
   return (
     <div>
       <Header/>
@@ -9,11 +14,12 @@ const Login = () => {
       </div>
       <div>
         <form className="bg-black text-white absolute mx-auto left-0 right-0 w-3/12 my-36 p-12 bg-opacity-80 rounded-sm">
-          <h1 className="font-bold text-3xl py-4">Sign In</h1>
-          <input className="p-2 w-full my-2 bg-gray-500" type="text" placeholder="Email Address"/>
-          <input className="p-2 w-full my-2 bg-gray-500" type="Password" placeholder="Password"/>
+          <h1 className="font-bold text-3xl py-4">{isSignIn===true?"Sign In":"Sign Up"}</h1>
+          {isSignIn===false && <input className="p-2 w-full my-2 bg-gray-600" type="text" placeholder="Full Name"/>}
+          <input className="p-2 w-full my-2 bg-gray-600" type="text" placeholder="Email Address"/>
+          <input className="p-2 w-full my-2 bg-gray-600" type="Password" placeholder="Password"/>
           <button className="p-3 w-full my-4 bg-red-800 rounded-sm">Sign In</button>
-          <p className="my-4">New to Netflix? Sign Up Now</p>
+          <p className="my-4 cursor-pointer" onClick={toggleSignUp}>{isSignIn===true ?"New to Netflix? Sign Up Now":"Already Regintered ,Sign In Now..."}</p>
         </form>
       </div>
     </div>
